666a36296f66b7a60923a1f503b177d339d124ee
phlummox <phlummox@users.noreply.github.com>
2021-03-29 10:43:46 +0000

    add GitHub CI workflow to build packer box

---
+++ publish.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# script to publish box to vagrant cloud.
+# shamelessly copied ^H^H^H adapted from
+# https://github.com/sdorsett/packer-centos7-esxi/blob/cb61c913911fafb35432eca4bd7cb532087ffa24/build-scripts/generic-packer-build-script.sh
+
+# expects VAGRANT_CLOUD_TOKEN
+# env var to be set in calling environmnent.
+
+set -euo pipefail
+set -x
+
+AUTHOR="$(grep '"Author":' templates/info.json | awk '{ print $2; }' | sed ' s/"//g; s/,//g; ')"
+BOX_NAME="$(make print_box_name)"
+BOX_VERSION="$(make print_box_version)"
+PROVIDER_TYPE="libvirt"
+
+false
+
+PATH_TO_BUILT_BOX="$(find output/ -name '*.box')"
+
+echo "ensuring vagrant-cloud box exists named ${AUTHOR}/$BOX_NAME has been created"
+curl https://vagrantcloud.com/api/v1/boxes \
+        -X POST \
+        -d "access_token=$VAGRANT_CLOUD_TOKEN" \
+        -d "box[username]=$AUTHOR" \
+        -d "box[name]=$BOX_NAME" \
+        -d "box[is_private]=false"
+
+echo "ensuring vagrant-cloud box named $AUTHOR/$BOX_NAME has version $BOX_VERSION created"
+curl "https://vagrantcloud.com/api/v1/box/$AUTHOR/$BOX_NAME/versions" \
+        -X POST \
+        -d "version[version]=$BOX_VERSION" \
+        -d "version[description]=new version" \
+        -d access_token="$VAGRANT_CLOUD_TOKEN"
+
+echo "ensuring vagrant-cloud box named $AUTHOR/$BOX_NAME has ${PROVIDER_TYPE} provider created"
+curl "https://vagrantcloud.com/api/v1/box/$AUTHOR/$BOX_NAME/version/$BOX_VERSION/providers" \
+-X POST \
+-d provider[name]="${PROVIDER_TYPE}" \
+-d access_token="$VAGRANT_CLOUD_TOKEN"
+
+echo "uploading $AUTHOR/$BOX_NAME vmware_desktop packer .box file"
+VAGRANT_CLOUD_PATH=$(curl -L "https://vagrantcloud.com/api/v1/box/$AUTHOR/$BOX_NAME/version/$BOX_VERSION/provider/${PROVIDER_TYPE}/upload?access_token=$VAGRANT_CLOUD_TOKEN" |cut -d "," -f1 | cut -d'"' -f4)
+echo "$VAGRANT_CLOUD_PATH"
+
+curl \
+  --progress-bar \
+  --verbose \
+  -X PUT --upload-file "$PATH_TO_BUILT_BOX" "$VAGRANT_CLOUD_PATH" > res || true
+
+cat -n res
+
+echo "releasing version $BOX_VERSION of $AUTHOR/$BOX_NAME $PROVIDER_TYPE"
+curl \
+  --progress-bar \
+  --verbose \
+  https://vagrantcloud.com/api/v1/box/$AUTHOR/$BOX_NAME/version/$BOX_VERSION/release -X PUT -d access_token="$VAGRANT_CLOUD_TOKEN" > release_result
+
+cat -n release_result
+
---
+++ vars.mk
@@ -0,0 +1,17 @@
+
+# version being built
+
+BOX_VERSION=0.0.1
+
+# packer config file to use
+PACKER_FILE=dokku.pkr.hcl
+
+# input box to use
+BASE_BOX_NAME=ubuntu2004
+BASE_BOX=generic/$(BASE_BOX_NAME)
+BASE_BOX_VERSION=3.2.6
+UBUNTU_IMG_PATH=$(HOME)/.vagrant.d/boxes/generic-VAGRANTSLASH-$(BASE_BOX_NAME)/$(BASE_BOX_VERSION)/libvirt/box.img
+
+# name for our built box
+BOX_NAME=ubuntu-dokku
+
